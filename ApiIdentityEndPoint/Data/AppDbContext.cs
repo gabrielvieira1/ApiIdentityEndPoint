de53e00b1980ede9b8699e21ce9e9f9ac4ac1eb7
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-07-09 00:16:15 -0300

    Project created with ASP NET Identity

---
+++ ApiIdentityEndPoint/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using ApiIdentityEndPoint.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiIdentityEndPoint.Data
+{
+  public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options);
+}
---
+++ ApiIdentityEndPoint/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ApiIdentityEndPoint.Entities
+{
+  public class User : IdentityUser
+  {
+    public string Document { get; set; } = string.Empty;
+  }
+}
